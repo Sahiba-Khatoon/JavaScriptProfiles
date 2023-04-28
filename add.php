b5dfa8360704100708fd65a5510b6e079887de9f
SahibaKhatoonNarejo <130455061+SahibaKhatoonNarejo@users.noreply.github.com>
2023-04-28 12:00:35 +0500

    Update add.php

--- add.php
+++ add.php
@@ -1,51 +1,70 @@
-<?php
-    require "pdo.php";
-    session_start();
+<?php 
 
-    if (!isset($_SESSION['name'])) {
-        die("ACCESS DENIED");
-    }
+    require_once "pdo.php";
+    session_start();
 
     if (isset($_POST['cancel'])) {
         header("Location: index.php");
         return;
     }
 
-    if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['headline']) && isset($_POST['summary'])  ) {
-        if (empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['email']) || empty($_POST['headline']) || empty($_POST['summary'])) {
-            $_SESSION['error'] = "All fields are required";
-            header("Location: add.php");
-            return;
-        }
+    if (isset($_POST['profile_id']) ) {
+        // if (empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['email']) || empty($_POST['headline']) || empty($_POST['summary'])) {
+        //     $_SESSION['error'] = "All fields are required";
+        //     header("Location: delete.php?profile_id=".$_GET['profile_id']);
+        //     return;
+        // }
 
-        $sql = "INSERT INTO profile (user_id, first_name, last_name, email, headline, summary) VALUES (:uid, :fn, :ln, :em, :he, :su)";
+        // $sql = "INSERT INTO profile (user_id, first_name, last_name, email, headline, summary) VALUES (:uid, :fn, :ln, :em, :he, :su)";
+
+        // $sql = "UPDATE profile SET first_name = :fn, last_name = :ln, email = :em , headline = :he, summary = :su WHERE profile_id = :pi";
+
+        $sql = "DELETE FROM profile WHERE profile_id = :pi";
 
         $statement = $pdo->prepare($sql);
 
         $statement->execute(array(
-            ":uid" => $_SESSION['user_id'],
-            ":fn" => $_POST['first_name'],
-            ":ln" => $_POST['last_name'],
-            ":em" => $_POST['email'],
-            ":he" => $_POST['headline'],
-            ":su" => $_POST['summary']
+            ":pi" => $_GET['profile_id']
         ));
 
-        $_SESSION['success'] = "Profile added";
+        $_SESSION['success'] = "Profile deleted";
         header("Location: index.php");
         return;
 
     }
-?>
+    
+    if (!isset($_GET['profile_id'])) {
+        $_SESSION['error'] = "Could not load profile";
+        header("Location: index.php");
+        return;
+    }
+    
+    $statement = $pdo->prepare("SELECT * FROM profile WHERE profile_id = :profile_id");
+    
+    $statement->execute(array(
+        ":profile_id" => $_GET["profile_id"],
+    ));
 
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if ($row == false) {
+        $_SESSION['error'] = "Could not load profile";
+        header("Location: index.php");
+        return;
+    }
+
+    $first_name = htmlentities($row["first_name"]);
+    $last_name = htmlentities($row["last_name"]);
+    $email = htmlentities($row["email"]);
+    $headline = htmlentities($row["headline"]);
+    $summary = htmlentities($row["summary"]);
+?>
 <!DOCTYPE html>
 <html>
 
 <body>
 
-    <h1>Adding Profile for <?php if (isset($_SESSION['name'])) {
-        echo $_SESSION['name']; 
-    } ?> </h1>
+    <h1>Deleteing Profile </h1>
 
     <?php 
         if (isset($_SESSION['error'])) {
@@ -54,23 +73,18 @@
         }
     ?>
 
+    <p>
+        First Name: <?php echo $first_name; ?>
+    </p>
+    <p>
+        Last Name: <?php echo $last_name; ?>
+    </p>
+
     <form method="POST">
         <p>
-            First Name: <input type="text" name="first_name" />
+            <input type="hidden" name="profile_id " value="<? $_GET['profile_id'] ?>" />
         </p>
-        <p>
-            Last Name: <input type="text" name="last_name" />
-        </p>
-        <p>
-            Email: <input type="text" name="email" />
-        </p>
-        <p>
-            Headline: <input type="text" name="headline" />
-        </p>
-        <p>
-            Summary: <textarea name="summary" rows="8" cols="80"></textarea>
-        </p>
-        <input type="submit" name="submit" value="Add" />
+        <input type="submit" name="delete" value="Delete" />
         <input type="submit" name="cancel" value="Cancel" />
     </form>
 </body>
